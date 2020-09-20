4832b8b246fef50ba16810cf2e98ea7bfd75e077
Pär Ericsson <ericsson@home.se>
2020-09-20 21:17:10 +0200

    Added DTOs and RouteDictionary

---
+++ EidelundBookStore/Models/DTOs/GridDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EidelundBookStore.Models.DTOs
+{
+    public class GridDTO
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string SortField { get; set; }
+        public string SortDirection { get; set; } = "asc";
+    }
+}
---
+++ EidelundBookStore/Models/Grid/FilterPrefix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EidelundBookStore.Models.Grid
+{
+    public static class FilterPrefix
+    {
+        public const string Genre = "genre-";
+        public const string Price = "price-";
+        public const string Author = "author-";
+    }
+}
